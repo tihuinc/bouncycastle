d9c46d5053b059be92fca4cbfe86612bb9ac97f1
Josh Lubaway <josh.lubaway@gmail.com>
2009-09-19 15:36:18 -0700

    Ensuring that subjects can not be prerequisites of themselves.

--- app/models/prerequisite.rb
+++ app/models/prerequisite.rb
@@ -3,4 +3,11 @@ class Prerequisite < ActiveRecord::Base
   belongs_to :prereq, :class_name => 'Subject'
 
   validates_presence_of :subject_id
+
+  validate :non_recursive_prereqs
+
+  private
+    def non_recursive_prereqs
+      errors.add_to_base('Subject can not be a prerequisite of itself') if subject_id == prereq_id
+    end
 end
--- test/unit/prerequisite_test.rb
+++ test/unit/prerequisite_test.rb
@@ -2,9 +2,18 @@ require 'test_helper'
 
 class PrerequisiteTest < ActiveSupport::TestCase
   def test_should_validate_presence_of_subject
-    pre = Prerequisite.new
-    assert !pre.valid?
+    p1 = Prerequisite.new
+    assert !p1.valid?
 
-    assert pre.valid?
+    p2 = Prerequisite.new(:subject_id => 1)
+    assert p2.valid?
+  end
+
+  def test_recursive_prerequisites_should_not_be_allowed
+    p1 = Prerequisite.new(:subject_id => 1, :prereq_id => 2)
+    assert p1.valid?
+
+    p2 = Prerequisite.new(:subject_id => 1, :prereq_id => 1)
+    assert !p2.valid?
   end
 end
