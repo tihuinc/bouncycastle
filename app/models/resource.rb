725e1088ec2c6f370ba261d5996c976f60ec3728
Josh Lubaway <josh.lubaway@gmail.com>
2009-09-27 13:25:34 -0700

    Adding Resource#upvote_count and Resource#downvote_count

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,15 +1,24 @@
 class Resource < ActiveRecord::Base
   belongs_to :subject
-  
+  has_many :votes
+
   validates_presence_of :subject_id
-  
+
   def uri
     uri = read_attribute(:uri)
-    
+
     if(uri.match(/^http:\/\//))
       uri
     else
       "http://" + uri
     end
+  end
+
+  def upvote_count
+    votes.count(:conditions => { :up => true })
+  end
+
+  def downvote_count
+    votes.count(:conditions => { :up => false })
   end
 end
--- test/unit/resource_test.rb
+++ test/unit/resource_test.rb
@@ -3,10 +3,31 @@ require 'test_helper'
 class ResourceTest < ActiveSupport::TestCase
   def test_URL_format
     resource = Resource.new(:uri => "google.com")
-    
+
     assert_equal "http://google.com", resource.uri
     resource.uri = "http://google.com"
     assert_equal "http://google.com", resource.uri
-    
+  end
+
+  def test_should_provide_upvote_count
+    resource = Resource.create(:subject_id => 1)
+    assert resource.valid?
+
+    resource.votes.build(:up => true)
+    resource.votes.build(:up => false)
+    resource.save
+
+    assert_equal(1, resource.upvote_count)
+  end
+
+  def test_should_provide_downvote_count
+    resource = Resource.create(:subject_id => 1)
+    assert resource.valid?
+
+    resource.votes.build(:up => true)
+    resource.votes.build(:up => false)
+    resource.save
+
+    assert_equal(1, resource.downvote_count)
   end
 end
